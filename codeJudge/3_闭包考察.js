638d89a24ccb1bafe9afadcf0474fd92c5b0d528
JinYang-Lai <young0053@163.com>
2022-02-21 11:47:19 +0800

    add new files

--- codeJudge/3_闭包考察.js
+++ codeJudge/3_闭包考察.js
@@ -8,10 +8,14 @@ var object = {
     };
   },
   getNameFunc2: function () {
+    console.log(this)
     return function () {
+      console.log(this.name)
       return this.name;
     };
   }
 };
 console.log(object.getNameFunc()()); // mac
 console.log(object.getNameFunc2()()); // undefined
+var func = object.getNameFunc2();
+console.log(func())
